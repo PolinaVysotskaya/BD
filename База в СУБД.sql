6ad0c26cc081af27069479ed669f23f539eed8f1
sasaafanaseva <112332319+sasaafanaseva@users.noreply.github.com>
2023-12-22 01:29:19 +0300

    База в СУБД.sql

--- База в СУБД.sql
+++ База в СУБД.sql
@@ -7,7 +7,7 @@ CREATE TABLE COUNTRY (
     id_country SERIAL PRIMARY KEY,
     timezone VARCHAR(255) DEFAULT ('GMT'),
     name_of_the_country VARCHAR(255) UNIQUE,
-    CONSTRAINT uq_country_name UNIQUE (name_of_the_country)
+    CONSTRAINT uq_country UNIQUE (name_of_the_country)
 );
 
 -- Создание таблицы Дети
@@ -18,7 +18,7 @@ CREATE TABLE CHILD (
     last_name VARCHAR(255) NOT NULL,
     age INTEGER check (age between 0 and 19),
     gender VARCHAR(10) check (gender = 'male' OR gender = 'female'),
-    CONSTRAINT uq_name UNIQUE (first_name, last_name), --?
+    CONSTRAINT uq_name UNIQUE (first_name, last_name),
     id_country SERIAL REFERENCES COUNTRY(id_country)
 );
 
@@ -30,8 +30,7 @@ CREATE TABLE ELF (
     last_name VARCHAR(255) NOT NULL,
     rating INTEGER check (rating >= 0),
     position VARCHAR(255) NOT NULL,
-    update_position DATE DEFAULT NOW(), -- новая позиция появилась сейчас, старая записалась (если была) с временем конца = сейчас
-    CONSTRAINT uq_elf_name UNIQUE (first_name, last_name), --?????
+    CONSTRAINT uq_elf_name UNIQUE (first_name, last_name),
     id_country SERIAL REFERENCES COUNTRY(id_country)
 );
 
@@ -57,11 +56,10 @@ CREATE TABLE GIFT (
 -- Создание таблицы Платежей
 DROP TABLE IF EXISTS PAYMENT CASCADE;
 CREATE TABLE PAYMENT (
-    id_payment SERIAL PRIMARY KEY,
+    id_payment INTEGER PRIMARY KEY REFERENCES DELIVERY(id_delivery),
     type_of_payment VARCHAR(255) check (type_of_payment = 'by cash' OR type_of_payment = 'by bank card'
-                                            OR type_of_payment = 'by a gift' OR type_of_payment = 'by sweets'),
-    "check" DECIMAL DEFAULT(0),
-    id_delivery SERIAL REFERENCES DELIVERY(id_delivery)
+                                            OR type_of_payment = 'by a gift' OR type_of_payment = 'by sweets' OR type_of_payment = 'by coins'),
+    "check" DECIMAL DEFAULT(0)
 );
 
 -- Создание таблицы Связь Детей и Подарков
@@ -76,23 +74,23 @@ CREATE TABLE CHILD_X_GIFT (
 DROP TABLE IF EXISTS CHILDREN_HISTORY CASCADE;
 CREATE TABLE CHILDREN_HISTORY
 (
-    id_child       SERIAL UNIQUE,
-    old_country    VARCHAR(255) NOT NULL,
-    old_age        INTEGER,
-    old_gender     CHAR(1),
-    CONSTRAINT pk_child_history PRIMARY KEY (id_child), --??????
-    CONSTRAINT fk_child_history_child FOREIGN KEY (id_child) REFERENCES new_year.CHILD(id_child) --?????
+    id_child INTEGER,
+    country VARCHAR(255) NOT NULL,
+    valid_from DATE DEFAULT NOW(),
+    valid_to DATE DEFAULT '2999-12-31',
+    PRIMARY KEY (id_child, valid_from)
 );
+
 
 -- Создание таблицы Изменений Эльфов
 DROP TABLE IF EXISTS ELF_HISTORY CASCADE;
 CREATE TABLE ELF_HISTORY
 (
-    id_elf SERIAL UNIQUE,
-    position_from DATE,
-    position_to DATE DEFAULT NOW(),
-    old_rating INTEGER,
-    old_position VARCHAR(255) NOT NULL,
-    CONSTRAINT pk_elf_history PRIMARY KEY (id_elf), --?????
-    CONSTRAINT fk_elf_history_elf FOREIGN KEY (id_elf) REFERENCES new_year.ELF(id_elf) --?????
+    id_elf INTEGER,
+    rating INTEGER check (rating >= 0),
+    position VARCHAR(255) NOT NULL,
+    pos_valid_from DATE DEFAULT NOW(),
+    pos_valid_to DATE DEFAULT '2999-12-31',
+    PRIMARY KEY (id_elf, pos_valid_from)
 );
+
